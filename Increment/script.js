42f5247e8171a875473523c9d18d374d90e8239e
Arman-Kabir <armankabir8@gmail.com>
2023-04-11 22:22:31 +0600

    increment done

---
+++ Increment/script.js
@@ -0,0 +1,16 @@
+let number = 0;
+let number1 = 0;
+
+const button = document.querySelector('#button');
+const display = document.getElementById('display');
+const button1 = document.querySelector('#button1');
+const display1 = document.getElementById('display1');
+
+button.addEventListener("click", () => {
+    number++;
+    display.textContent = number;
+});
+button1.addEventListener("click", () => {
+    number1++;
+    display1.textContent = number1;
+});
