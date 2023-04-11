fd4723b29df6de38364cf50eef6f33c753146d66
Arman-Kabir <armankabir8@gmail.com>
2023-04-11 23:41:45 +0600

    core react state

--- Increment/increment.js
+++ Increment/increment.js
@@ -2,6 +2,45 @@
 // console.log(React);
 // console.log(ReactDOM);
 
-const domContainer  = document.querySelector('#root');
+const domContainer = document.querySelector('#root');
 
-ReactDOM.render('Hello World',domContainer);
+// const myElement = React.createElement("div", null, React.createElement("p", null, "Hello World"));
+
+// const myElement = React.createElement("div", null, [
+//     React.createElement("p",null,"Hello World"),
+//     React.createElement("p",null,"Hello World")
+// ]);
+
+const Increment = () => {
+    const [counter, setCounter] = React.useState(0);
+    
+    return (
+        <div>
+            <h1 id="display">{counter}</h1>
+            <div>
+                <button id="button" onClick={()=>setCounter(counter + 1)}>Increment +</button>
+            </div>
+        </div>
+    )
+}
+
+
+
+
+
+// const myElement = (
+//     <div>
+//         <h1 id="display">0</h1>
+//         <div>
+//             <button id="button">Increment +</button>
+//         </div>
+//     </div>
+// );
+
+ReactDOM.render(< Increment />, domContainer);
+
+
+
+// let p = document.createElement("p");
+// p.innerHTML = "Hello World";
+// domContainer.appendChild(p);
